272dbdbf5b40f803654c079c06929838d2c57a15
KarthikeyanBommannan <karthibommannan@gmail.com>
2023-09-28 01:28:08 +0530

    HI

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st 
+import os 
 
-uploaded_files = st.file_uploader("Choose a file",type=["PDF","XML"])
-submitted = st.button("Submit")
+uploaded_files = st.sidebar.file_uploader("Choose a file",type=["PDF","XML","TXT"])
+submitted = st.sidebar.button("Submit")
 if submitted:
-    print(uploaded_files)
+    directory = r"C:\Users\karthikeyan\OneDrive\Desktop\Notepad"
+    path = os.path.join(directory,uploaded_files.name)
+    with open(path,"rb") as f:
+        data = f.read()
+        data = data.decode("utf-8")
+        st.write(data)
     
     
-# for uploaded_file in uploaded_files:
-#     bytes_data = uploaded_file.read()
-#     st.write("filename:", uploaded_file.name)
-#     st.write(bytes_data)
-
--- model.py
+++ model.py
@@ -1,23 +1,10 @@
-import os
-import warnings
-warnings.simplefilter("ignore")
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatAnthropic
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessages
 
-directory = r"E:\\"
-
-key_path = os.path.join(directory,"openapikey.txt")
-
-try:
-    with open(key_path,"r") as f:
-        openapi_key = f.read()
-        # print(openapi_key)
-except FileNotFoundError:
-    print(f"{key_path} not found")
-except Exception as e:
-    print(f"An error occured {e}")
-    
-os.environ['OPENAI_API_KEY'] = openapi_key
-
-llm = OpenAI(temperature = 0.6)
-name = llm(input())
-print(name)
+chat = ChatAnthropic()
