28add7fe1e1a98fcdc6996e4008a1e837ce7bcdf
KarthikeyanBommannan <karthibommannan@gmail.com>
2023-09-28 02:32:08 +0530

    new

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import os 
 
-uploaded_files = st.sidebar.file_uploader("Choose a file",type=["PDF","XML","TXT"])
+uploaded_files = st.sidebar.file_uploader("Choose a file",type=["PDF"])
 submitted = st.sidebar.button("Submit")
 if submitted:
     directory = r"C:\Users\karthikeyan\OneDrive\Desktop\Notepad"
