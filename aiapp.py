19fa628fe24c9c45cb473ac67c817e4bfbf3c095
KarthikeyanBommannan <karthibommannan@gmail.com>
2023-09-30 18:19:59 +0530

    final

--- aiapp.py
+++ aiapp.py
@@ -7,6 +7,7 @@ from  langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+# from htmlTemplates import css, bot_template, user_template
  
 
 def get_pdf_text(document):
@@ -30,27 +31,45 @@ def get_text_chunks(text):
 
 
 def get_vetorstore(text_chunks):
-    #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl"     )
+    embeddings = OpenAIEmbeddings()
+    #embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl"     )
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key="chat_history",max_history=10) 
+    memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True,max_history=5) 
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
+                                                                llm=llm,
+                                                                retriever=vectorstore.as_retriever(),
+                                                                memory=memory
+                                                             )
     return conversation_chain
+
+
+def handle_userinput(user_question):
+     response = st.session_state.conversation({"question":user_question})
+     st.write(response["answer"])
+
+
+
+
 
 
 def main():
     load_dotenv()
     st.set_page_config(layout="wide",page_title="Chat with PDF",page_icon=":books:")
+    # st.write(css,unsafe_allow_html=True)
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
     st.header("Chat with PDF")
-    st.text_input("Ask your Question about your documents")
+    user_question = st.text_input("Ask your Question about your documents")
+    if user_question:
+        handle_userinput(user_question)
+    
+    # st.write(user_template.replace("{{MSG}}","Hello"),unsafe_allow_html=True)
+    # st.write(bot_template.replace("{{MSG}}","Hello"),unsafe_allow_html=True)
     
     with st.sidebar:
         document = st.file_uploader(":green[Upload a PDF file]",type = ["PDF"],accept_multiple_files=True)
@@ -65,7 +84,7 @@ def main():
                 # create vector store 
                 vectorstore = get_vetorstore(text_chunks)  
                 
-                conversation = get_conversation_chain(vectorstore)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 
